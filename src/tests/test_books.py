05375d038360439a55a76d6f54fc7172e1ad3937
Heath Brown <heathdbrown@users.noreply.github.com>
2022-09-04 13:19:48 -0500

    Add failing test

--- src/tests/test_books.py
+++ src/tests/test_books.py
@@ -19,3 +19,25 @@ def test_create_book(test_app, monkeypatch):
 def test_create_book_invalid_json(test_app):
     response = test_app.post("/books/", data=json.dumps({"title": "The Eye of the World"}))
     assert response.status_code == 422
+
+def test_read_book(test_app, monkeypatch):
+    test_data = {"id": 1, "title": "The Eye of the World", "authors": "Robert Jordan"}
+
+    async def mock_get(id):
+        return test_data
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.get("/books/1")
+    assert response.status_code == 200
+    assert response.json() == test_data
+
+def test_read_book_incorrect_id(test_app, monkeypatch):
+    async def mock_get(id):
+        return None
+
+    monkeypatch.setattr(crud, "get", mock_get)
+
+    response = test_app.get("/books/999")
+    assert response.status_code == 404
+    assert response.json()["detail"] == "book not found"
