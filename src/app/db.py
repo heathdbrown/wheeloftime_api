391a2747a9638215e86d7b9690d33493795869d0
Heath Brown <heathdbrown@users.noreply.github.com>
2022-09-04 12:03:25 -0500

    Add books model

--- src/app/db.py
+++ src/app/db.py
@@ -9,5 +9,19 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 metadata = MetaData()
 
+
+
+# SQLAlchemy
+engine = create_engine(DATABASE_URL)
+metadata = MetaData()
+
+books = Table(
+    "books",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("title", String(100)),
+    Column("authors", String(100)),
+)
+
 # databases query builder
 database = Database(DATABASE_URL)
