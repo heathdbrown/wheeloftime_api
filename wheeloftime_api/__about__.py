def3dfcc4a7f42acab2ebdc22d5717bab7a2e928
Heath Brown <heathd.brown@gmail.com>
2022-11-06 22:07:31 -0600

    hatch init files

---
+++ wheeloftime_api/__about__.py
@@ -0,0 +1,4 @@
+# SPDX-FileCopyrightText: 2022-present Heath Brown <heathd.brown@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+__version__ = '0.0.1'
---
+++ wheeloftime_api/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2022-present Heath Brown <heathd.brown@gmail.com>
+#
+# SPDX-License-Identifier: MIT
